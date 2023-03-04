58444cc8a507e9475e2591adf3f7a3f7fae19952
bayuajii <ajib210601@gmail.com>
2023-03-04 13:34:53 +0700

    portofolioi tailwindcssl

---
+++ dist/js/script.js
@@ -0,0 +1,21 @@
+// navbar-fixed
+window.onscroll = function () {
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+
+    if (window.pageYOffset > fixedNav) {
+        header.classList.add('navbar-fixed');
+    }
+    else {
+        header.classList.remove('navbar-fixed')
+    }
+}
+
+// hamburger
+const hamberger = document.querySelector('#humberger');
+const navMenu = document.querySelector('#nav-menu');
+
+hamberger.addEventListener('click', function () {
+    hamberger.classList.toggle('hamberger-active');
+    navMenu.classList.toggle('hidden');
+})
